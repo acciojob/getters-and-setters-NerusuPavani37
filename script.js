5fd17ceef3b539504bc73df63abb2c7a0b1316f3
Pavani <136723974+NerusuPavani37@users.noreply.github.com>
2025-04-12 12:33:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+		this.name=name;
+		this.age=age;
+	}
 
-class Student extends Person {}
+	getName(){
+		return this.name;
+	}
+	setAge(age){
+		this.age=age;
+	}
+}
 
-class Teacher extends Person {}
+class Student extends Person {
+  constructor(name,age){
+    super(name,age);
+  }
+	study(){
+		let name = super.getName();
+		console.log(name + " is studying")
+	}
+}
+
+class Teacher extends Person {
+   constructor(name,age){
+    super(name,age);
+  }
+	teach(){
+		let name = super.getName();
+		console.log(name + " is teaching");
+	}
+}
+
+let student=new Student("Pavani",24);
+student.study();
+let teacher = new Teacher("Accio", 50);
+teacher.teach();
+teacher.setAge(40);
+console.log(teacher.age);
+
 
 // Do not change the code below this line
 window.Person = Person;
